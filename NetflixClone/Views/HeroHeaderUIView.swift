8013fb68a4c855912523c85255459f2f095c1861
Berkay Tuncel <berkaytncl98@gmail.com>
2023-09-07 16:06:11 +0300

    Add tableview a custom header view and images

---
+++ NetflixClone/Views/HeroHeaderUIView.swift
@@ -0,0 +1,84 @@
+//
+//  HeroHeaderUIView.swift
+//  NetflixClone
+//
+//  Created by Berkay Tuncel on 6.09.2023.
+//
+
+import UIKit
+
+final class HeroHeaderUIView: UIView {
+    
+    private let downloadButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Download", for: .normal)
+        button.layer.borderColor = UIColor.systemBackground.cgColor
+        button.layer.borderWidth = 1
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.cornerRadius = 5
+        return button
+    }()
+
+    private let playButton: UIButton = {
+       let button = UIButton()
+        button.setTitle("Play", for: .normal)
+        button.layer.borderColor = UIColor.systemBackground.cgColor
+        button.layer.borderWidth = 1
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.cornerRadius = 5
+        return button
+    }()
+    
+    private let heroImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = UIImage(named: "heroImage")
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(heroImageView)
+        addGradient()
+        addSubview(playButton)
+        addSubview(downloadButton)
+        applyConstraint()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        heroImageView.frame = bounds
+    }
+    
+    private func addGradient() {
+        let gradiantLayer = CAGradientLayer()
+        gradiantLayer.colors = [
+            UIColor.clear.cgColor,
+            UIColor.systemBackground.cgColor
+        ]
+        gradiantLayer.frame = bounds
+        layer.addSublayer(gradiantLayer)
+    }
+    
+    private func applyConstraint() {
+        let playButtonConstraints = [
+            playButton.trailingAnchor.constraint(equalTo: centerXAnchor, constant: -10),
+            playButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -50),
+            playButton.widthAnchor.constraint(equalToConstant: 100)
+        ]
+        
+        let downloadButtonConstraints = [
+            downloadButton.leadingAnchor.constraint(equalTo: centerXAnchor, constant: 10),
+            downloadButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -50),
+            downloadButton.widthAnchor.constraint(equalToConstant: 100)
+        ]
+        
+        NSLayoutConstraint.activate(playButtonConstraints)
+        NSLayoutConstraint.activate(downloadButtonConstraints)
+    }
+}

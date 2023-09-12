483ce258ee7f2db67489cebea1aa9be9d9177bad
Berkay Tuncel <berkaytncl98@gmail.com>
2023-09-12 23:48:31 +0300

    Create UpcomingMedia model

---
+++ NetflixClone/Models/UpcomingMedia.swift
@@ -0,0 +1,13 @@
+//
+//  UpcomingMedia.swift
+//  NetflixClone
+//
+//  Created by Berkay Tuncel on 12.09.2023.
+//
+
+import Foundation
+
+struct UpcomingMedia {
+    let title: String
+    let posterPath: String
+}

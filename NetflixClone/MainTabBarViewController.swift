79b1c810ad872a4eae4a5f7748e24d0ba1f0b90e
Berkay Tuncel <berkaytncl98@gmail.com>
2023-09-13 03:36:50 +0300

    Change tabBarItem title

--- NetflixClone/MainTabBarViewController.swift
+++ NetflixClone/MainTabBarViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainTabBarViewController.swift
 //  NetflixClone
 //
 //  Created by Berkay Tuncel on 5.09.2023.
@@ -20,7 +20,7 @@ final class MainTabBarViewController: UITabBarController {
         
         vc1.tabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName: "house"), tag: 0)
         vc2.tabBarItem = UITabBarItem(title: "Coming Soon", image: UIImage(systemName: "play.circle"), tag: 1)
-        vc3.tabBarItem = UITabBarItem(title: "Top Search", image: UIImage(systemName: "magnifyingglass"), tag: 2)
+        vc3.tabBarItem = UITabBarItem(title: "Search", image: UIImage(systemName: "magnifyingglass"), tag: 2)
         vc4.tabBarItem = UITabBarItem(title: "Downloads", image: UIImage(systemName: "arrow.down.to.line"), tag: 3)
         
         tabBar.tintColor = .label
